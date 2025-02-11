9a1a71a768f2e852d7ecca64637832b124bd4bd0
Aditya Saroha <143502508+Aditya-138-12@users.noreply.github.com>
2025-02-11 06:32:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <ModelViewer />
-        <div className='logo'>Technotsav Logo</div>
-        {<p className='pw' style={{ position: "absolute", bottom: 0, left: 0, marginLeft: "20px" }}>Made with ❤️ by Technotsav Web Team</p >}
-        <Apply />
-        <Links facebook={"https://www.github.com/Aditya-138-12"} />
-      </BrowserRouter>
+      <p>This is the status site of the technotsav.tech, the UI would be like any other status. sites.</p>
     </>
 
   );
